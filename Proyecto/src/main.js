33a312803c1bbc7820b26e91bb841292de8f7717
Angel Caamal <angelcamalc@gmail.com>
2026-05-21 22:16:28 -0500

    se arregló pantallaso cuando se recarga la pagina

--- Proyecto/src/main.js
+++ Proyecto/src/main.js
@@ -1,25 +1,10 @@
-import "./CSS/style.css";
-import "./CSS/landing.css";
-import './CSS/style.css'
-import './CSS/hero.css'
-import './CSS/variables.css'
-import './CSS/slider.css';
 import './JS/slider.js';
-import './CSS/galeria.css';
 import './JS/galeria.js';
-import './CSS/video.css';
-import './CSS/comida.css';
 import './JS/comidas.js';
-import './CSS/medicina.css';
 import './JS/medicina.js';
-import './CSS/cultura.css';
 import './JS/cultura.js';
-import './CSS/comentarios.css';
 import './JS/comentarios.js';
 import './JS/loganSlider.js'
-import './CSS/leyendas.css'
-import './CSS/artesanias.css'
-import './CSS/comunidades.css'
 
 // ===== TÍTULO DE LA PÁGINA =====
 document.title = "K’ajóol Lu’um";
--- backend/index.js
+++ backend/index.js
@@ -25,9 +25,9 @@ const PORT = 4000;
 
 const mysqlConnection = mysql.createConnection({
   host: "127.0.0.1",
-  port: 3307,
+  port: 3306,
   user: "root",
-  password: "Mysql123*",
+  password: "angelcamalc(29052008)",
   database: "productos_web"
 });
 
